887fbd2553ccb044b98b1c4126b75e7b44cf8ff2
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2016-04-05 11:05:07 -0700

    Small refactor of adjusted height/width, add consts
    * Refactor adjusted height calculations to use aspectRatio
    
    * Change maxWidth and maxHeight to consts
    
    * Change aspectRatio to const, replace "onevar" style with repeated keywords
    
    "onevar" style replaced for improved git friendliness

--- app/helpers/twitter-image-resizer.js
+++ app/helpers/twitter-image-resizer.js
@@ -1,42 +1,43 @@
 import Ember from 'ember';
 
 export function twitterImageResizer(width, height) {
-  let adjustedHeight,
-      adjustedWidth,
-      maxHeight = 1200,
-      maxWidth = 600,
-      aspectRatio = width / height;
+  let adjustedHeight;
+  let adjustedWidth;
+
+  const ASPECT_RATIO = width / height;
+  const MAX_HEIGHT = 1200;
+  const MAX_WIDTH = 600;
 
   if (!width || !height) {
     throw new Error('Twitter Image Resizer: height and width required');
   }
 
-  if (width <= maxWidth && height <= maxHeight) {
+  if (width <= MAX_WIDTH && height <= MAX_HEIGHT) {
     return {
       adjustedWidth: width,
       adjustedHeight: height,
     };
   }
 
-  if (aspectRatio >= 1.5) {
-    adjustedHeight = maxWidth * height / width;
-    adjustedWidth = Math.round(adjustedHeight) * aspectRatio;
+  if (ASPECT_RATIO >= 1.5) {
+    adjustedHeight = MAX_WIDTH / ASPECT_RATIO;
+    adjustedWidth = Math.round(adjustedHeight) * ASPECT_RATIO;
 
-    if (adjustedWidth > maxWidth) {
-      adjustedWidth = maxWidth;
+    if (adjustedWidth > MAX_WIDTH) {
+      adjustedWidth = MAX_WIDTH;
     }
-  } else if (aspectRatio > 1) {
-    adjustedHeight = maxWidth * height / width;
-    adjustedWidth = adjustedHeight * aspectRatio;
-  } else if (aspectRatio <= 1 && aspectRatio > 0.5) {
-    adjustedWidth = maxWidth;
-    adjustedHeight = adjustedWidth * height / width;
-  } else if (aspectRatio <= 0.5) {
-    adjustedWidth = maxHeight * aspectRatio;
-    adjustedHeight = Math.round(adjustedWidth) * height / width;
+  } else if (ASPECT_RATIO > 1) {
+    adjustedHeight = MAX_WIDTH / ASPECT_RATIO;
+    adjustedWidth = adjustedHeight * ASPECT_RATIO;
+  } else if (ASPECT_RATIO <= 1 && ASPECT_RATIO > 0.5) {
+    adjustedWidth = MAX_WIDTH;
+    adjustedHeight = adjustedWidth / ASPECT_RATIO;
+  } else if (ASPECT_RATIO <= 0.5) {
+    adjustedWidth = MAX_HEIGHT * ASPECT_RATIO;
+    adjustedHeight = Math.round(adjustedWidth) / ASPECT_RATIO;
 
-    if (adjustedHeight > maxHeight) {
-      adjustedHeight = maxHeight;
+    if (adjustedHeight > MAX_HEIGHT) {
+      adjustedHeight = MAX_HEIGHT;
     }
   }
 
