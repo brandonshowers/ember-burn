a7844c1bb3cff0ae5a7351e6904bb5fc26861088
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2016-04-04 18:03:47 -0700

    Fix twitter resizing algorithm further

--- app/helpers/twitter-image-resizer.js
+++ app/helpers/twitter-image-resizer.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function twitterImageResizer(width, height) {
   let adjustedHeight,
       adjustedWidth,
-      maxHeight = 600,
+      maxHeight = 1200,
       maxWidth  = 600;
 
   if (!width || !height) {
@@ -15,32 +15,35 @@ export function twitterImageResizer(width, height) {
   let aspectRatio = width / height;
   // aspectRatio = Math.round(aspectRatio * 10000) / 10000;
 
-  if (height > maxHeight || width > maxWidth) {
-    if (aspectRatio >= 1.5) {
-      adjustedHeight = 600 * height / width;
-      adjustedWidth = Math.round(adjustedHeight) * aspectRatio;
-
-      if (adjustedWidth > 600) {
-        adjustedWidth = 600;
-      }
-    } else if (aspectRatio > 1) {
-      adjustedHeight = 600 * height / width;
-      adjustedWidth = adjustedHeight * aspectRatio;
-    } else if (aspectRatio <= 1 && aspectRatio > 0.5) {
-      adjustedWidth = 600;
-      adjustedHeight = adjustedWidth * height / width;
-    } else if (aspectRatio <= 0.5) {
-      adjustedWidth = 1200 * aspectRatio;
-      adjustedHeight = Math.round(adjustedWidth) * height / width;
-
-      if (adjustedHeight > 1200) {
-        adjustedHeight = 1200;
-      }
-    }
-  } else {
-    adjustedWidth = width;
-    adjustedHeight = height;
+  if (width <= maxWidth && height <= maxHeight) {
+    return {
+      adjustedWidth: width,
+      adjustedHeight: height,
+    };
   }
+
+  if (aspectRatio >= 1.5) {
+    adjustedHeight = 600 * height / width;
+    adjustedWidth = Math.round(adjustedHeight) * aspectRatio;
+
+    if (adjustedWidth > 600) {
+      adjustedWidth = 600;
+    }
+  } else if (aspectRatio > 1) {
+    adjustedHeight = 600 * height / width;
+    adjustedWidth = adjustedHeight * aspectRatio;
+  } else if (aspectRatio <= 1 && aspectRatio > 0.5) {
+    adjustedWidth = 600;
+    adjustedHeight = adjustedWidth * height / width;
+  } else if (aspectRatio <= 0.5) {
+    adjustedWidth = 1200 * aspectRatio;
+    adjustedHeight = Math.round(adjustedWidth) * height / width;
+
+    if (adjustedHeight > 1200) {
+      adjustedHeight = 1200;
+    }
+  }
+
 
   return {
     adjustedWidth: Math.round(adjustedWidth),
